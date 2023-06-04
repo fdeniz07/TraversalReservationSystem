9cf4497ff5d0d8a258dd60b21bfe93d6bcbe41d9
Fatih Deniz <deniz.fatih@hotmail.de>
2023-06-04 23:57:32 +0200

    DefaultController

---
+++ Traversal.MVC/Controllers/DefaultController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Traversal.MVC.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
